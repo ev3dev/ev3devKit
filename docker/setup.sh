5b630ed066793ff403403e3d10989553e8f3f000
David Lechner <david@lechnology.com>
2019-04-26 22:54:30 -0500

    docker: simplify setup script
    don't ask for build directory, like other ev3dev projects

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,15 +4,9 @@ set -e
 
 script_dir=$(dirname $(readlink -f "$0"))
 
-build_dir="$1"
-if [ ! -n "$build_dir" ]; then
-    echo "Error: Must specify build directory"
-    exit 1
-fi
-
-case $2 in
+case $1 in
     armel|armhf)
-        arch=$2
+        arch=$1
         ;;
     *)
         echo "Error: Must specify 'armel' or 'armhf'"
@@ -25,21 +19,24 @@ if ! which docker >/dev/null; then
     exit 1
 fi
 
+build_dir="build-$arch"
 image_name="ev3devkit-$arch"
 container_name="ev3devkit_$arch"
+
+mkdir -p $build_dir
 
 docker build \
     --tag $image_name \
     --no-cache \
     --file "$script_dir/$arch.dockerfile" \
     "$script_dir/"
-mkdir -p $build_dir
 
 docker rm --force $container_name >/dev/null 2>&1 || true
 docker run \
     --volume "$(readlink -f $build_dir):/build" \
     --volume "$(pwd):/src" \
     --workdir /build \
+    --user $(id -u):$(id -g) \
     --name $container_name \
     --env "TERM=$TERM" \
     --env "DESTDIR=/build/dist" \
